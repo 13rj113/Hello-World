5f4cb239b68941b424f57d8a151ac68ccab0a0a3
13rj113 <162026499@qq.com>
2016-04-11 19:59:34 +0800

    Second

---
+++ src/example/Add.java
@@ -0,0 +1,13 @@
+package example;
+
+public class Add {
+	int s = 0;
+	public void cal(){
+		int a = 1,b = 1;
+		s = a + b;
+	}
+	public void show(){
+		int l = s;
+		System.out.println(l);
+	}
+}
---
+++ src/example/MainActivity.java
@@ -0,0 +1,14 @@
+package example;
+
+public class MainActivity {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add a = new Add();
+		a.cal();
+		System.out.print("计算结果为：");
+		a.show();
+	}
+}
